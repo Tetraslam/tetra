da0cb61365ad613c9ac5c08b0976a378381a0217
Tetraslam <bhowmickshresht@gmail.com>
2021-06-21 12:05:17 +0530

    Update __init__.py

--- tetra/__init__.py
+++ tetra/__init__.py
@@ -1,14 +1,2 @@
-from typing import final
-from questionary import question
-from rich.console import Console
-from rich.text import Text
-from rich.progress import Progress
-import os
-import time
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame
-from pygame import mixer
-from time import sleep
-import questionary
-from cryptography.fernet import Fernet
-from questionary import Style
+from tetraslam_pkg import tetra
+
